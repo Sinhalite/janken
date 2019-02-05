ca24e7dc3e51c696f7d1dde9017fbc65874a9e31
Sinhalite <sinhalite@example.com>
2019-02-05 13:23:26 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      player_hand: null,
+      isSubmitted: false,
+      results: {}
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  componentDidMount() {
+
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    if (this.state.player_hand === null) {
+      this.setState({ error: "手を選択してください" })
+      return
+    }
+
+    this.setState({ error: null })
+    this.setState({ isSubmitted: true })
+
+    axios.post("https://623c7v9nm9.execute-api.ap-northeast-1.amazonaws.com/v1", {
+      hand: this.state.player_hand
+    }).then(res => {
+      this.setState({ results: res.data.body })
+    })
+  }
+
+  onClick(e, value) {
+    this.setState({ player_hand: value })
+    Array.prototype.slice.call(document.getElementsByClassName("active")).forEach(function (node) {
+      node.classList.remove("active")
+    });
+    e.currentTarget.classList.add("active")
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>無駄に壮大なじゃんけんアプリ</h1>
         </header>
+        <div id="button-area">
+          <div className="hand-button" onClick={(e) => this.onClick(e, 0)}>グー</div>
+          <div className="hand-button" onClick={(e) => this.onClick(e, 1)}>チョキ</div>
+          <div className="hand-button" onClick={(e) => this.onClick(e, 2)}>パー</div>
+        </div>
+        <div id="error">{this.state.error}</div>
+        {this.state.isSubmitted &&
+          <div id="result">
+            <p>
+              CPUの出した手は…<br />
+              {this.state.results.cpu_hand}
+            </p>
+            <p>
+              {this.state.results.result}
+            </p>
+          </div>
+        }
+        <form onSubmit={this.onSubmit}>
+          <button type="submit" className="submit-button">勝負</button>
+        </form>
       </div>
     );
   }
